326fbc0c9e83528ab1bdf365ca330dd100ae90e2
knttnk <61683744+knttnk@users.noreply.github.com>
2026-01-09 16:39:18 +0900

    github pagesの設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 静的サイトとしてエクスポート
+  output: "export",
+
+  // GitHub Pages では _next の最適化機能を利用できないため無効
+  images: {
+    unoptimized: true
+  },
+
+  ...(isProd && {
+    basePath: "/my-website",
+    assetPrefix: "/my-website/"
+  })
 };
 
 export default nextConfig;
