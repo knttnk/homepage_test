14cf3a34582a8faca23a4c8e6d510ba5d65fab64
knttnk <61683744+knttnk@users.noreply.github.com>
2026-01-17 04:53:21 +0000

    json5を使うように

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,16 +2,21 @@ import { getRequestConfig } from 'next-intl/server';
 import { hasLocale } from 'next-intl';
 import { routing } from './routing';
 
+import json5 from 'json5';
+import fs from 'fs/promises';
+import path from 'path';
+
 export default getRequestConfig(async ({ requestLocale }) => {
-    // Typically corresponds to the `[locale]` segment
-    const requested = await requestLocale;
-    const locale = hasLocale(routing.locales, requested)
-        ? requested
-        : routing.defaultLocale;
+	// Typically corresponds to the `[locale]` segment
+	const requested = await requestLocale;
+	const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
 
-    return {
-        locale,
-        messages: (await import(`../messages/${locale}.json`)).default
-    };
+	const filePath = path.join(process.cwd(), 'messages', `${locale}.json5`);
+	const fileContents = await fs.readFile(filePath, 'utf8');
+	const messages = json5.parse(fileContents);
+
+	return {
+		locale,
+		messages,
+	};
 });
-
