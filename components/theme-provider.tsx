ad89e473dcdf344795f904e21c3467c7e9134acc
knttnk <61683744+knttnk@users.noreply.github.com>
2026-04-07 02:12:31 +0000

    バグの原因を特定したので追加

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -8,6 +8,7 @@ import {
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   return (
+    // BUG: https://github.com/shadcn-ui/ui/issues/10200 が解決するまではエラーが出る
     <NextThemesProvider enableSystem storageKey="intentui-theme" {...props}>
       {children}
     </NextThemesProvider>
